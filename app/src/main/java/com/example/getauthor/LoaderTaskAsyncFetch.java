767047aadb443c1e635f93ff34074c767d7a01db
allw1n <charlesallwin31@gmail.com>
2022-04-13 17:01:33 +0530

    started AsyncTaskLoader

--- app/src/main/java/com/example/getauthor/LoaderTaskAsyncFetch.java
+++ app/src/main/java/com/example/getauthor/LoaderTaskAsyncFetch.java
@@ -1,4 +1,29 @@
 package com.example.getauthor;
 
-public class LoaderTaskAsyncFetch {
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+public class LoaderTaskAsyncFetch extends AsyncTaskLoader<String> {
+
+    String query;
+
+    public LoaderTaskAsyncFetch(@NonNull Context context, String query) {
+        super(context);
+        this.query = query;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return null;
+    }
 }
